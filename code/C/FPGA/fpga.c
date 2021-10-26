613317cba142dd4c78cf8de00b1cd09c9d6b5935
Jesús Chacón <jcsombria@gmail.com>
2021-10-26 19:29:33 +0200

    getcwd

--- code/C/FPGA/fpga.c
+++ code/C/FPGA/fpga.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     printf("usage: %s filename", argv[0]);
+    return 1;
+  }
+  char cwd[100];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf("Current working dir: %s\n", cwd);
+  } else {
+    perror("getcwd() error");
     return 1;
   }
   FILE *fp = fopen(argv[1], "r");
